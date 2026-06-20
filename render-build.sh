8c5cae5063122d31b0bf1130d1d2184fdf2bdbf4
Sufyan Khan <sufyankhans072@gmail.com>
2026-06-20 14:14:58 +0100

    all updated backend

--- render-build.sh
+++ render-build.sh
@@ -2,14 +2,17 @@
 # exit on error
 set -o errexit
 
-# Install node dependencies
+# 1. Install node dependencies
 npm install
 
-# Create bin directory if it doesn't exist
+# 2. Create bin directory if it doesn't exist
 mkdir -p .bin
 
-# Download Linux yt-dlp binary
+# 3. Download Linux yt-dlp binary
 curl -L https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp -o .bin/yt-dlp
 
-# Make it executable
+# 4. Make it executable
 chmod a+rx .bin/yt-dlp
+
+# 5. Safely append to PATH just for this application runtime environment
+export PATH="$PATH:$(pwd)/.bin"
